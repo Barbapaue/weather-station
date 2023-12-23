2f35932e7afb66c77dc104e208b4d162ae2200f1
PaoloPasianot <115974374+PaoloPasianot@users.noreply.github.com>
2023-12-23 12:28:05 +0100

    Initial commit

---
+++ src/main/kotlin/paolopasianot/it/Application.kt
@@ -0,0 +1,17 @@
+package paolopasianot.it
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import paolopasianot.it.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
---
+++ src/main/kotlin/paolopasianot/it/plugins/MQTT.kt
@@ -0,0 +1,2 @@
+package paolopasianot.it.plugins
+

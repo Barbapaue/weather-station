0191713a995373a5f7e883ab793a419036241c75
PaoloPasianot <115974374+PaoloPasianot@users.noreply.github.com>
2023-12-23 18:03:37 +0100

    forward

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,3 +46,30 @@ dependencies {
 
     implementation("com.github.traxterz:ktor-plugin-mqtt:v1.1.0")
 }
+
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
+
+    docker {
+        jreVersion.set(JavaVersion.VERSION_17)
+        localImageName.set("sample-docker-image")
+        imageTag.set("0.0.1-preview")
+        portMappings.set(listOf(
+            io.ktor.plugin.features.DockerPortMapping(
+                80,
+                8080,
+                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+            )
+        ))
+
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "ktor-app" },
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
+            )
+        )
+    }
+}
